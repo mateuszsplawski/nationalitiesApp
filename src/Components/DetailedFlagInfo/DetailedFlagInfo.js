dcea9ddecd3b6c38f86642dce25f6bb09408b321
Mateusz <mathiskk@gmail.com>
2020-02-21 21:03:24 +0100

    design update

--- src/Components/DetailedFlagInfo/DetailedFlagInfo.js
+++ src/Components/DetailedFlagInfo/DetailedFlagInfo.js
@@ -3,26 +3,70 @@ import styled from "styled-components";
 
 const StyledWrapper = styled.main`
   width: 100vw;
-  height: 100vh;
+  min-height: 100vh;
   display: flex;
-  justify-content: center;
   align-items: center;
-  flex-direction: column;
+  justify-content: center;
 
-  h1 {
+  section {
     display: flex;
+    justify-content: center;
+    align-items: center;
     flex-direction: column;
-    text-align: center;
+    box-shadow: 0 0 30px -20px #121212;
+    background: whitesmoke;
+    padding: 20px;
 
-    span {
-      font-size: 1rem;
+    h1 {
+      display: flex;
+      flex-direction: column;
+      text-align: center;
+      font-size: 25px;
+
+      span {
+        font-size: 20px;
+      }
     }
-  }
 
-  img {
-    width: 120px;
-    height: 70px;
-    object-fit: cover;
+    img {
+      width: 400px;
+      height: 200px;
+      object-fit: scale-down;
+    }
+
+    ul {
+      padding: 0;
+      display: flex;
+      flex-wrap: wrap;
+      width: 500px;
+      justify-content: space-around;
+      align-items: center;
+      position: relative;
+
+      li {
+        width: 250px;
+        padding: 20px;
+        list-style: none;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        font-size: 18px;
+        font-weight: lighter;
+
+        strong {
+          font-size: 20px;
+        }
+      }
+    }
+    @media (max-width: 500px) {
+      img {
+        width: 300px;
+      }
+      ul {
+        width: 300px;
+      }
+    }
   }
 `;
 
@@ -40,7 +84,7 @@ const DetailedFlagInfo = ({ match }) => {
   return (
     <StyledWrapper>
       {countryData && (
-        <>
+        <section>
           <h1>
             {countryData.name}
             <span>{countryData.nativeName}</span>
@@ -48,22 +92,22 @@ const DetailedFlagInfo = ({ match }) => {
           <img src={countryData.flag} alt={countryData.name + " flag"} />
           <ul>
             <li>
-              Stolica: <strong>{countryData.capital}</strong>
+              Stolica<strong>{countryData.capital}</strong>
             </li>
             <li>
-              Region: <strong>{countryData.region}</strong>
+              Region<strong>{countryData.region}</strong>
             </li>
             <li>
-              Podregion: <strong>{countryData.subregion}</strong>
+              Podregion<strong>{countryData.subregion}</strong>
             </li>
             <li>
-              Populacja: <strong>{countryData.population}</strong> osób
+              Populacja<strong>{countryData.population} osób</strong>
             </li>
             <li>
-              Powierzchnia: <strong>{countryData.area}</strong> m²
+              Powierzchnia<strong>{countryData.area} m²</strong>
             </li>
             <li>
-              Waluta:{" "}
+              Waluta
               <strong>
                 {countryData.currencies[0].name} /{" "}
                 {countryData.currencies[0].code}
@@ -77,7 +121,7 @@ const DetailedFlagInfo = ({ match }) => {
               </strong>
             </li>
           </ul>
-        </>
+        </section>
       )}
     </StyledWrapper>
   );

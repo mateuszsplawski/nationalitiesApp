f50a7036369c9efae01e2ad7422230250ba8aaa1
Mateusz <mathiskk@gmail.com>
2020-02-21 20:29:39 +0100

    updated routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from "react";
 import { Route, Switch } from "./../node_modules/react-router-dom";
 import NationalitiesApp from "./Components/NationalitiesApp";
-import CountryInfo from "./Components/DetailedFlagInfo/DetailedFlagInfo";
+import DetailedFlagInfo from "./Components/DetailedFlagInfo/DetailedFlagInfo";
 import "./App.css";
 
 const App = () => {
   return (
     <Switch>
       <Route path={"/"} exact component={NationalitiesApp} />
-      <Route path="/:country" render={props => <CountryInfo {...props} />} />
+      <Route
+        path="/NationalitiesApp/:country"
+        render={props => <DetailedFlagInfo {...props} />}
+      />
     </Switch>
   );
 };
--- src/Components/DetailedFlagInfo/DetailedFlagInfo.js
+++ src/Components/DetailedFlagInfo/DetailedFlagInfo.js
@@ -8,28 +8,27 @@ const StyledWrapper = styled.main`
   justify-content: center;
   align-items: center;
   flex-direction: column;
-`;
 
-const H1 = styled.h1`
-  display: flex;
-  flex-direction: column;
-  text-align: center;
+  h1 {
+    display: flex;
+    flex-direction: column;
+    text-align: center;
 
-  span {
-    font-size: 1rem;
+    span {
+      font-size: 1rem;
+    }
   }
-`;
 
-const Img = styled.img`
-  width: 120px;
-  height: 70px;
-  object-fit: cover;
+  img {
+    width: 120px;
+    height: 70px;
+    object-fit: cover;
+  }
 `;
 
 const DetailedFlagInfo = ({ match }) => {
   const country = match.params.country;
-  const [countryData, setCountryData] = useState({});
-  const [renderResults, setRenderResults] = useState(false);
+  const [countryData, setCountryData] = useState(undefined);
 
   useEffect(() => {
     const API = `https://restcountries.eu/rest/v2/name/`;
@@ -38,51 +37,48 @@ const DetailedFlagInfo = ({ match }) => {
       .then(data => setCountryData(...data))
       .catch(err => console.log(err));
   }, []);
-
-  useEffect(() => {
-    (Object.keys(countryData).length === 0 &&
-      countryData.constructor === Object) === false
-      ? setRenderResults(true)
-      : setRenderResults(false);
-  }, [countryData]);
   return (
     <StyledWrapper>
-      <H1>
-        {countryData.name}
-        <span>{countryData.nativeName}</span>
-      </H1>
-      <Img src={countryData.flag} />
-      <ul>
-        <li>
-          Stolica: <strong>{countryData.capital}</strong>
-        </li>
-        <li>
-          Region: <strong>{countryData.region}</strong>
-        </li>
-        <li>
-          Podregion: <strong>{countryData.subregion}</strong>
-        </li>
-        <li>
-          Populacja: <strong>{countryData.population}</strong> osób
-        </li>
-        <li>
-          Powierzchnia: <strong>{countryData.area}</strong> m²
-        </li>
-        <li>
-          Waluta:{" "}
-          <strong>
-            {renderResults && countryData.currencies[0].name} /{" "}
-            {renderResults && countryData.currencies[0].code}
-          </strong>
-        </li>
-        <li>
-          Język:{" "}
-          <strong>
-            {renderResults && countryData.languages[0].name} /{" "}
-            {renderResults && countryData.languages[0].nativeName}
-          </strong>
-        </li>
-      </ul>
+      {countryData && (
+        <>
+          <h1>
+            {countryData.name}
+            <span>{countryData.nativeName}</span>
+          </h1>
+          <img src={countryData.flag} alt={countryData.name + " flag"} />
+          <ul>
+            <li>
+              Stolica: <strong>{countryData.capital}</strong>
+            </li>
+            <li>
+              Region: <strong>{countryData.region}</strong>
+            </li>
+            <li>
+              Podregion: <strong>{countryData.subregion}</strong>
+            </li>
+            <li>
+              Populacja: <strong>{countryData.population}</strong> osób
+            </li>
+            <li>
+              Powierzchnia: <strong>{countryData.area}</strong> m²
+            </li>
+            <li>
+              Waluta:{" "}
+              <strong>
+                {countryData.currencies[0].name} /{" "}
+                {countryData.currencies[0].code}
+              </strong>
+            </li>
+            <li>
+              Język:{" "}
+              <strong>
+                {countryData.languages[0].name} /{" "}
+                {countryData.languages[0].nativeName}
+              </strong>
+            </li>
+          </ul>
+        </>
+      )}
     </StyledWrapper>
   );
 };
